dfc430861d6d39fcf0805e2f34bf2d5fe2fa456f
nosia-80 <anatoliynosenko80@gmail.com>
2025-03-25 11:55:22 +0200

    Добавляет стили для футера

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,8 +1,10 @@
 const galleryPicsEl = document.querySelectorAll('.gallery__pic');
 let activePic = document.querySelector('.gallery__pic--active');
 
-galleryPicsEl.forEach(pic => {
-  pic.addEventListener('mouseover', () => {
+galleryPicsEl.forEach(picture => {
+  picture.addEventListener('mouseover', event => {
+    const pic = event.currentTarget;
+
     if (pic !== activePic) {
       pic.classList.add('gallery__pic--active');
       activePic.classList.remove('gallery__pic--active');
