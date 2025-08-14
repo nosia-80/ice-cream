8a18c0f69c387de8c3b4fb36c515c5b6c960df6d
nosia-80 <anatoliynosenko80@gmail.com>
2025-08-14 10:55:01 +0000

    Deploying to gh-pages from @ nosia-80/ice-cream@c2aaf9160838ce89f3201da9d44851cb8ea3e298 🚀

--- index.439a3d7d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelectorAll(".gallery__pic");let c=document.querySelector(".gallery__pic--active");e.forEach((e=>{e.addEventListener("mouseover",(e=>{const t=e.currentTarget;t!==c&&(t.classList.add("gallery__pic--active"),c.classList.remove("gallery__pic--active"),c=t)}))}));
-//# sourceMappingURL=index.439a3d7d.js.map
